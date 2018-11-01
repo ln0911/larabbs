44f026675b239ee401c801a92975c2b5e66a6200
lining <lining4885@163.com>
2018-11-01 13:07:35 +0800

    Add custom helpers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,5 @@
 <?php
-
+include_once __DIR__."/helpers.php";
 /*
 |--------------------------------------------------------------------------
 | Create The Application
---
+++ bootstrap/helpers.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lining
+ * Date: 2018/11/1
+ * Time: 1:05 PM
+ */
