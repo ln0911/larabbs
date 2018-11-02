b3dbc3ad8078c17f9aa8e61d54993a2eb2aafd13
vagrant <vagrant@homestead>
2018-11-02 09:04:09 +0000

    Notify author when topic replied

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -49,5 +52,30 @@ class User extends Authenticatable
     public function isAuthorOf($model)
     {
         return $this->id == $model->user_id;
+    }
+
+    /**
+     * Reply Notify
+     * @param $instance
+     */
+    public function notify($instance)
+    {
+        if($this->id == Auth::id()) {
+            return;
+        }
+        $this->increment('notification_count');
+        $this->laravelNotify($instance);
+    }
+
+    /**
+     * Read Message
+     */
+    public function markAsRead()
+    {
+        $this->notification_count =0;
+        $this->save();
+
+        $this->unreadNotifications->markAsRead();
+
     }
 }
