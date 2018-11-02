f72869d95b974dc61d7498ef93f3e9bcabd9fc77
vagrant <vagrant@homestead>
2018-11-02 01:35:08 +0000

    topic order

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'Larabbs') - Laravel 进阶教程</title>
+    <title>@yield('title', 'Larabbs') - 个人博客</title>
 
 <!-- Styles -->
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
