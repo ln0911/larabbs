acbb85a112e8cf047bb85207c37ed0cd3fe3dffd
vagrant <vagrant@homestead>
2018-11-02 05:02:17 +0000

    add editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
 
 <!-- Styles -->
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('style')
 </head>
 
 <body>
@@ -40,5 +41,6 @@
 
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
+@yield("scripts")
 </body>
 </html>
