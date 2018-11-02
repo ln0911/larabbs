3fdbcd1569009aa30fdef7bfc94bf87f65a01826
vagrant <vagrant@homestead>
2018-11-02 01:08:12 +0000

    Topic index page

---
+++ resources/views/topics/_sidebar.blade.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lining
+ * Date: 2018/11/2
+ * Time: 8:50 AM
+ */
+?>
+<div class="panel panel-default">
+    <div class="panel-body">
+        右边导航栏
+    </div>
+</div>
