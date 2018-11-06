ddb496c1637a12060ef34c16cd136cd81fc4da89
vagrant <vagrant@homestead>
2018-11-06 06:00:08 +0000

    user can register using phone

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','introduction','avatar'
+        'name', 'email', 'phone','password','introduction','avatar'
     ];
 
     /**
